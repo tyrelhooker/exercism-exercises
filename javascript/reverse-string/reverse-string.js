26f3b2c7a45f9082ee84d29bf63c54ca0b8c34a8
TJ Hooker <tyrelhooker@gmail.com>
2021-07-20 23:28:00 -0400

    Clean up comments.

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,34 +1,3 @@
 export const reverseString = inputString => inputString.split('').reverse().join('');
-  // return reversedString;
-
-
-  // let a = '';
-  // let b = '';
-  // let c = ''; 
-
-  // for (const char of [...inputString]) {
-  //   if (!a) {
-  //     a = char;
-  //   } else {
-  //     b = char;
-  //     c = b.concat(a);
-  //     a = c;
-  //   }
-  // }
-  // // [...inputString].map((char) => {
-  // //   if (!a) {
-  // //     a = char;
-  // //   } else {
-  // //     b = char;
-  // //     c = b.concat(a);
-  // //     a = c;
-  // //   }
-  // // })
-  // return c;
-// }
-// let start = Date.now();
-// console.log(reverseString('Ramen'));
-// let end = Date.now();
-// console.log(`Execution took: ${end - start} ms`);
 
 
