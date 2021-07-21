ce9eaec59b37f25b143648b0f1f4d291ca5a567a
TJ Hooker <tyrelhooker@gmail.com>
2021-07-20 23:27:26 -0400

    Refactor code base to provide single line return of values using string and array methods chained

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,24 +1,12 @@
-// export 
-const reverseString = (inputString) => {
-  
-  // const reversedString = inputString.split('').reverse().join('');
+export const reverseString = inputString => inputString.split('').reverse().join('');
   // return reversedString;
 
 
-  let a = '';
-  let b = '';
-  let c = ''; 
+  // let a = '';
+  // let b = '';
+  // let c = ''; 
 
-  for (const char of [...inputString]) {
-    if (!a) {
-      a = char;
-    } else {
-      b = char;
-      c = b.concat(a);
-      a = c;
-    }
-  }
-  // [...inputString].map((char) => {
+  // for (const char of [...inputString]) {
   //   if (!a) {
   //     a = char;
   //   } else {
@@ -26,12 +14,21 @@ const reverseString = (inputString) => {
   //     c = b.concat(a);
   //     a = c;
   //   }
-  // })
-  return c;
-}
-let start = Date.now();
-console.log(reverseString('Ramen'));
-let end = Date.now();
-console.log(`Execution took: ${end - start} ms`);
+  // }
+  // // [...inputString].map((char) => {
+  // //   if (!a) {
+  // //     a = char;
+  // //   } else {
+  // //     b = char;
+  // //     c = b.concat(a);
+  // //     a = c;
+  // //   }
+  // // })
+  // return c;
+// }
+// let start = Date.now();
+// console.log(reverseString('Ramen'));
+// let end = Date.now();
+// console.log(`Execution took: ${end - start} ms`);
 
 
