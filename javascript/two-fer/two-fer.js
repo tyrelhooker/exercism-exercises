ef99b20e6bb51b35e0c4a860e562e60d1a8040ef
TJ Hooker <tyrelhooker@gmail.com>
2021-07-20 23:17:35 -0400

    Update exercism exercises to version 2

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,9 +1,6 @@
 
-// function twoFer(name = 'you') {
-//   return `One for ${name}, one for me.`;
-// }
-
-let twoFer = (name = 'you') => `One for ${name}, one for me.`;
-
+function twoFer(name = 'you') {
+  return `One for ${name}, one for me.`;
+}
 
 module.exports = { twoFer };
