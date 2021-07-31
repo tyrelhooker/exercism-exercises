5753aade616fac9e2f325ccb11ec12869722ae25
TJ Hooker <tyrelhooker@gmail.com>
2021-07-31 15:58:52 -0400

    Refactor code

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,35 +1,38 @@
 export class Triangle {
   constructor(...sides) {
-    this.sides = sides;
+    this.sides = sides.sort((a, b) => a - b);
+  }
+
+  checkForTriangleInequality() {
+    let [side1, side2, side3] = this.sides;
+
+    if (side1 + side2 > side3) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  checkSideEquality() {
+    return this.sides.some((elem, index) => this.sides.indexOf(elem) !== index);
   }
 
   get isEquilateral() {
     return this.sides.every((elem, index, arr) => elem === arr[0] && elem !== 0)
-    
   }
 
   get isIsosceles() {
-    let [side1, side2, side3] = this.sides;
-
-    const twoSidesEqual = this.sides.some((elem, index) => this.sides.indexOf(elem) !== index);
-
-    if (twoSidesEqual && (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)) {
+    if (this.checkSideEquality() && this.checkForTriangleInequality()) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   }
 
   get isScalene() {
-    let [side1, side2, side3] = this.sides;
-
-    const noSidesEqual = !this.sides.some((elem, index) => this.sides.indexOf(elem) !== index);
-
-    if (noSidesEqual && (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)) {
+    if (!this.checkSideEquality() && this.checkForTriangleInequality()) {
       return true;
-    } 
-    else {
+    } else {
       return false
     }
   }
