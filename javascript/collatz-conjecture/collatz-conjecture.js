74971ffce944da5743078e293c4b99511aac0fc8
TJ Hooker <tyrelhooker@gmail.com>
2021-07-31 23:29:28 -0400

    Create logic to determine even and odd input numbers. Refactor code to use recursive function and default parameter for count

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,24 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error('Remove this statement and implement this function');
+export const steps = (number) => {
+  if (number <= 0) {
+    throw 'Only positive numbers are allowed';
+  }
+
+  const handleSequence = (number, count=0) => {
+    if (number === 1) {
+      return count
+    } else if (number%2 === 0) {
+      number = number/2;
+      count += 1;
+      return handleSequence(number, count);
+    } else {
+      number = number*3 + 1;
+      count += 1;
+      return handleSequence(number, count);
+    }
+  }
+
+  return handleSequence(number);
 };
